bf5b0cbc0b60ce24d5483e5c4d53122860963373
draolo <34096335+draolo@users.noreply.github.com>
2021-12-01 17:28:55 +0100

    fixed melee weapons balanced basketbomb

--- Assets/Scripts/BulletList/Bullet2BombScript.cs
+++ Assets/Scripts/BulletList/Bullet2BombScript.cs
@@ -5,39 +5,31 @@ using System.Collections.Generic;
 
 public class Bullet2BombScript : AbstractBulletExplosive
 {
-    public float ExplosionDelay = 3f;
+    public float ExplosionDelay = 2f;
 
-    new void Start()
+    private new void Start()
     {
         base.Start();
         rb.velocity = transform.right * speed;
         StartCoroutine(ExplodeWithDelay(ExplosionDelay));
     }
 
-
-
-
-    IEnumerator ExplodeWithDelay(float delay)
+    private IEnumerator ExplodeWithDelay(float delay)
     {
         yield return new WaitForSeconds(delay);
         ExplodeCircle();
     }
 
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
         {
             var enemy = collision.gameObject.GetComponent<PlayerHealth>();
             if (enemy)
             {
                 float vel = GetComponent<Rigidbody2D>().velocity.magnitude;
-                if (vel > 5)
-                    enemy.CmdTakeDamage(Mathf.CeilToInt(vel / 3), shootedBy);
+                if (vel > 12)
+                    enemy.CmdTakeDamage(Mathf.CeilToInt(vel / 12), shootedBy);
             }
         }
     }
-
-
-  
 }
